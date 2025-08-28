1a2d3cd7334c5f17db9872112be38f7caff8e27b
6604062630200 <s6604062630200@email.kmutnb.ac.th>
2025-08-28 23:00:40 +0700

    feat(products-page): add category list component and mock data feat(ui): add prompt font

--- frontend/src/app/(public)/products/page.tsx
+++ frontend/src/app/(public)/products/page.tsx
@@ -1,6 +1,7 @@
 "use client"
 
-import { ProductList } from "@/components/common/List"
+import { CategoryList, ProductList } from "@/components/common/List"
+import { categories } from "@/data/mock/category_mock"
 import { products } from "@/data/mock/product_mock"
 
 const ProductsPage = () => {
@@ -8,6 +9,7 @@ const ProductsPage = () => {
     <div className="max-w-6xl mx-auto">
       <div className="py-8 px-4 md:px-2 xl:px-0 md:py-16">
         <h1 className="font-bold text-2xl md:text-4xl mb-12">สินค้าทั้งหมด</h1>
+        <CategoryList categories={categories} />
         <ProductList products={products} />
       </div>
     </div>
--- frontend/src/components/common/List.tsx
+++ frontend/src/components/common/List.tsx
@@ -1,9 +1,15 @@
 "use client"
 import { Product } from "@/types/product_type"
 import { CardProduct } from "./Card"
+import { CategoryButton } from "./Button"
+import { Category } from "@/types/category_type"
 
 interface ProductListProps {
   products: Product[]
+}
+
+interface CategoryListProps {
+  categories: Category[]
 }
 
 export const SuggestList = ({ products }: ProductListProps) => {
@@ -21,6 +27,23 @@ export const ProductList = ({ products }: ProductListProps) => {
     <div className="grid grid-cols-2 md:grid-cols-3 xl:grid-cols-4 gap-4 md:gap-5 xl:gap-6">
       {products.map((p) => (
         <CardProduct key={p.id} product={p} />
+      ))}
+    </div>
+  )
+}
+
+export const CategoryList = ({ categories }: CategoryListProps) => {
+  return (
+    <div className="flex flex-wrap py-2 mb-6 gap-2">
+      {categories.map((c, index) => (
+        <CategoryButton
+          key={index}
+          children={c.children}
+          onClick={c.onClick}
+          href={c.href || ""}
+          active={c.active || false}
+          className={c.className || ""}
+        />
       ))}
     </div>
   )
