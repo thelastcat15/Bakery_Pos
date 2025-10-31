bb26e92eb5f17239b89e62ea7f3aa102aea027d5
6604062630200 <s6604062630200@email.kmutnb.ac.th>
2025-10-31 15:06:20 +0700

    add tracking icon

--- frontend/src/components/layout/Navbar.tsx
+++ frontend/src/components/layout/Navbar.tsx
@@ -67,6 +67,25 @@ const Navbar = () => {
               </span>
             )}
           </li>
+
+          {userLoaded && user && (
+            <li className="relative">
+              <Link href={`tracking`}>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                  className="size-6 text-amber-500">
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="m7.875 14.25 1.214 1.942a2.25 2.25 0 0 0 1.908 1.058h2.006c.776 0 1.497-.4 1.908-1.058l1.214-1.942M2.41 9h4.636a2.25 2.25 0 0 1 1.872 1.002l.164.246a2.25 2.25 0 0 0 1.872 1.002h2.092a2.25 2.25 0 0 0 1.872-1.002l.164-.246A2.25 2.25 0 0 1 16.954 9h4.636M2.41 9a2.25 2.25 0 0 0-.16.832V12a2.25 2.25 0 0 0 2.25 2.25h15A2.25 2.25 0 0 0 21.75 12V9.832c0-.287-.055-.57-.16-.832M2.41 9a2.25 2.25 0 0 1 .382-.632l3.285-3.832a2.25 2.25 0 0 1 1.708-.786h8.43c.657 0 1.281.287 1.709.786l3.284 3.832c.163.19.291.404.382.632M4.5 20.25h15A2.25 2.25 0 0 0 21.75 18v-2.625c0-.621-.504-1.125-1.125-1.125H3.375c-.621 0-1.125.504-1.125 1.125V18a2.25 2.25 0 0 0 2.25 2.25Z"
+                  />
+                </svg>
+              </Link>
+            </li>
+          )}
           <li>
             <Link href={`/login`}>
               <span>
@@ -140,6 +159,13 @@ const Navbar = () => {
           <li>
             <Link
               href="/cart"
+              className="block w-full py-3 text-center hover:bg-amber-100 rounded">
+              Cart
+            </Link>
+          </li>
+          <li>
+            <Link
+              href="/tracking"
               className="block w-full py-3 text-center hover:bg-amber-100 rounded">
               Cart
             </Link>
